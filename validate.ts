6dc7c05d26977e1ceb838428b0a5e17ad4bb89f2
Chris Campbell <chris.c@buildkite.com>
2023-11-07 14:15:01 +1100

    Add buildkite pipeline for validating pipelines

---
+++ validate.ts
@@ -0,0 +1,39 @@
+import { parse, stringify } from "npm:yaml";
+import { Validator } from "npm:jsonschema";
+import { globSync } from "npm:glob";
+import chalk from "npm:chalk";
+import fs from "node:fs";
+import process from "node:process";
+
+const response = await fetch(
+  "https://raw.githubusercontent.com/buildkite/pipeline-schema/main/schema.json"
+);
+const schema = await response.json();
+
+var v = new Validator();
+const errors = [];
+
+const pipelines = globSync("pipelines/*/pipeline.yaml");
+pipelines.forEach(async (pipeline) => {
+  const raw = fs.readFileSync(pipeline, "utf8");
+  const parsed = parse(raw);
+  const res = v.validate(parsed, schema);
+
+  if (!res.valid) {
+    const firstError = res.errors[0];
+    const message = `${firstError.property} ${firstError.message}`;
+
+    errors.push({ pipeline, message });
+  }
+});
+
+if (errors.length > 0) {
+  console.error("Errors found:");
+  errors.forEach((error) => {
+    console.error(
+      `${chalk.bgRed(error.pipeline)}: ${chalk.red(error.message)}`
+    );
+  });
+}
+
+process.exit(errors.length > 0 ? 1 : 0);
