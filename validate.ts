d9f603b524054dc2d1882ee3a983c8d01c06e6ef
Chris Campbell <chris.c@buildkite.com>
2023-11-20 14:12:01 +1100

    Clean up validate script

--- validate.ts
+++ validate.ts
@@ -1,20 +1,21 @@
-import { parse, stringify } from "npm:yaml";
+import { parse } from "npm:yaml";
 import { Validator } from "npm:jsonschema";
 import { globSync } from "npm:glob";
 import chalk from "npm:chalk";
 import fs from "node:fs";
 import process from "node:process";
 
+// Fetch the current pipeline configuration schema
 const response = await fetch(
   "https://raw.githubusercontent.com/buildkite/pipeline-schema/main/schema.json"
 );
 const schema = await response.json();
 
-var v = new Validator();
-const errors = [];
+const v = new Validator();
+const errors: { pipeline: string; message: string }[] = [];
 
 const pipelines = globSync("pipelines/*/pipeline.yaml");
-pipelines.forEach(async (pipeline) => {
+pipelines.forEach((pipeline) => {
   const raw = fs.readFileSync(pipeline, "utf8");
   const parsed = parse(raw);
   const res = v.validate(parsed, schema);
