02c905137b53436b248e01af42de4a559b5edd2f
Oly Su <taiya1229@gmail.com>
2023-12-15 16:43:04 +1100

    🌱 Add example xcode project To make it easier for users to set up their project and run the pipeline

---
+++ fastlane-ios/myProject/myProject/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  myProject
+//
+//  Created by Buildkite on 12/14/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
