e93806e5aedb04f2973f0ea590ee347b7ad3e902
Chris Campbell <chris.c@buildkite.com>
2024-02-05 17:42:10 +1100

    Add external dep

--- golang-ci/example-project/main_test.go
+++ golang-ci/example-project/main_test.go
@@ -1,56 +1,11 @@
 package main
 
 import (
-	"net/http"
-	"net/http/httptest"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
-func TestHelloHandler(t *testing.T) {
-	req, err := http.NewRequest("GET", "/hello", nil)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	rr := httptest.NewRecorder()
-	handler := http.HandlerFunc(helloHandler)
-
-	handler.ServeHTTP(rr, req)
-
-	if status := rr.Code; status != http.StatusOK {
-		t.Errorf("handler returned wrong status code: got %v want %v",
-			status, http.StatusOK)
-	}
-}
-
-func TestHelloHandler404(t *testing.T) {
-	req, err := http.NewRequest("GET", "/wrongpath", nil)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	rr := httptest.NewRecorder()
-	handler := http.HandlerFunc(helloHandler)
-
-	handler.ServeHTTP(rr, req)
-
-	if status := rr.Code; status != http.StatusNotFound {
-		t.Errorf("Handler returned wrong status code: got %v want %v", status, http.StatusNotFound)
-	}
-}
-
-func TestHelloHandlerMethodNotAllowed(t *testing.T) {
-	req, err := http.NewRequest("POST", "/hello", nil)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	rr := httptest.NewRecorder()
-	handler := http.HandlerFunc(helloHandler)
-
-	handler.ServeHTTP(rr, req)
-
-	if status := rr.Code; status != http.StatusMethodNotAllowed {
-		t.Errorf("Handler returned wrong status code: got %v want %v", status, http.StatusMethodNotAllowed)
-	}
+func TestEqual(t *testing.T) {
+	assert.Equal(t, 1, 1)
 }
