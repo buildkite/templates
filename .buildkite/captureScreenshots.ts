9ce7ddc6026a68b300ad0e921f006d66968bd1b6
Chris Campbell <chris.c@buildkite.com>
2024-04-04 13:42:59 +1100

    Update script for docker

--- .buildkite/captureScreenshots.ts
+++ .buildkite/captureScreenshots.ts
@@ -8,9 +8,15 @@ try {
 } catch (e) {}
 
 // Launch the browser and open a new blank page
-const browser = await puppeteer.launch({ headless: "new" });
+const browser = await puppeteer.launch({
+  args: [
+    "--no-sandbox",
+    "--font-render-hinting=none",
+    "--force-color-profile=srgb",
+  ],
+});
 const page = await browser.newPage();
-page.setViewport({ width: 1920, height: 1080, deviceScaleFactor: 2 });
+await page.setViewport({ width: 1920, height: 1080, deviceScaleFactor: 2 });
 
 console.log("Fetching screenshots...");
 for (const pipeline of globSync("*/pipeline.yaml")) {
@@ -18,7 +24,8 @@ for (const pipeline of globSync("*/pipeline.yaml")) {
 
   console.log(templateID);
   await page.goto(
-    `https://buildkite.com/pipelines/playground/embed?tid=${templateID}`
+    `https://buildkite.com/pipelines/playground/embed?tid=${templateID}`,
+    { waitUntil: "networkidle0" }
   );
   await page.waitForSelector(".react-flow__node");
 
