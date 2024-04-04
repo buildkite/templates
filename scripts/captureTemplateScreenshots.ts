2f44e9ca73b3361ce85e51666c5f3953b1c6da9c
Chris Campbell <chris.c@buildkite.com>
2024-04-04 11:52:18 +1100

    Add script for capturing screenshots

---
+++ scripts/captureTemplateScreenshots.ts
@@ -0,0 +1,29 @@
+import puppeteer from "https://deno.land/x/puppeteer@16.2.0/mod.ts";
+import { dirname } from "https://deno.land/std@0.205.0/path/dirname.ts";
+import { globSync } from "npm:glob";
+
+try {
+  Deno.mkdirSync("screenshots");
+} catch (e) {}
+
+// Launch the browser and open a new blank page
+const browser = await puppeteer.launch({ headless: "new" });
+const page = await browser.newPage();
+page.setViewport({ width: 1920, height: 1080, deviceScaleFactor: 2 });
+
+console.log("Fetching screenshots...");
+for (const pipeline of globSync("*/pipeline.yaml")) {
+  const templateID = dirname(pipeline);
+
+  console.log(templateID);
+  await page.goto(
+    `https://buildkite.com/pipelines/playground/embed?tid=${templateID}`
+  );
+  await page.waitForSelector(".react-flow__node");
+  await page.screenshot({
+    path: `screenshots/${templateID}.png`,
+    omitBackground: true,
+  });
+}
+
+await browser.close();
